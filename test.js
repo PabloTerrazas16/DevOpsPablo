aadb2f1bf74b9bc833f01ea289ce45892824f62b
unknown <SSDD@POAPCA02LC0230.duoc.acad>
2026-03-20 21:04:43 -0300

    6to commit

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
 const mensaje = "hola";
-if(mensaje==="hola")
-    {console.log("Test aprobado");
-        process.exit(0);
 
-    }else{
+if( mensaje==="hola") {
+        console.log("Test aprobado");
+    } else {
         console.log("Test fallido");
-        process.exit(1);
     }
